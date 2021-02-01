a14b2868d7a0edb0eb0f82ac57dd899e747efa0d
dpetrova <daniella_petrova@abv.bg>
2021-02-01 11:14:42 +0200

    intro to vue 3

---
+++ 00_Intro to Vue 3/main.js
@@ -0,0 +1,15 @@
+//Create Vue instance
+
+const app = Vue.createApp({
+  data() {
+    return {
+      premium: true,
+      cart: []
+    }
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id)
+    }
+  }
+})
