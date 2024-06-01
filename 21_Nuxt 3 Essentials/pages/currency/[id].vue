0ce849d90d010a4e7082759e670c55fb4f4b8143
dpetrova <daniella_petrova@abv.bg>
2024-06-01 18:40:44 +0300

    nuxt 3 essentials

---
+++ 21_Nuxt 3 Essentials/pages/currency/[id].vue
@@ -0,0 +1,26 @@
+<template>
+  <template>
+    <div>
+      <h2>{{ coin.name }} Detail page</h2>
+      <table border="1 px solid">
+        <thead>
+          <th>Symbol</th>
+          <th>Rank</th>
+          <th>Price - US $</th>
+          <th>Market Cap - US $</th>
+        </thead>
+        <tr>
+          <td>{{ coin.symbol }}</td>
+          <td>{{ coin.rank }}</td>
+          <td>{{ coin.price_usd }}</td>
+          <td>{{ coin.market_cap_usd }}</td>
+        </tr>
+      </table>
+    </div>
+  </template>
+</template>
+
+<script setup>
+const route = useRoute();
+const { data: coin } = await useFetch('/api/ticker?id=' + route.params.id);
+</script>
